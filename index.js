9478dc65b4cc335dd2c7af472c4595517dcd0b47
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-13 18:14:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function theBeatlesPlay (musician, instrument){
 i=0
 for (i=0; i<musician.length;i+=1){
   array.push('musician[i]" plays "instrument[i]')
-}
+i +=1}
 return array
 }
 // add solution here
